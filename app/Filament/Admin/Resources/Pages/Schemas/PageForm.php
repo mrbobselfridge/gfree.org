9902e194aa44e756ba224f4897c1f4658f65c5ed
mrbobselfridge <selfridge@gmail.com>
2026-06-12 23:08:17 -0400

    now

--- app/Filament/Admin/Resources/Pages/Schemas/PageForm.php
+++ app/Filament/Admin/Resources/Pages/Schemas/PageForm.php
@@ -10,6 +10,7 @@ use App\Models\Page;
 use App\Rules\HttpOrRelativeUrl;
 use App\Rules\PageSlugPath;
 use App\Rules\ValidPageParent;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
