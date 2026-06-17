2c958c7b6c9dc9b3824d94fe01739b1287f8c549
mrbobselfridge <selfridge@gmail.com>
2026-06-17 17:45:41 -0400

    2026-06-17 17:45:40

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -41,7 +42,8 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->navigationGroups([
-                'Website',
+                NavigationGroup::make('Website')
+                    ->collapsible(false),
             ])
             ->navigationItems([
                 NavigationItem::make('User Manual')
