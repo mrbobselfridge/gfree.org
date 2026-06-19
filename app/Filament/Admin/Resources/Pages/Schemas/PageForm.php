c5529e441d32f7349db3f3dc9176a9c0325507ac
mrbobselfridge <selfridge@gmail.com>
2026-06-19 11:11:53 -0400

    2026-06-19 11:11:50

--- app/Filament/Admin/Resources/Pages/Schemas/PageForm.php
+++ app/Filament/Admin/Resources/Pages/Schemas/PageForm.php
@@ -184,7 +184,7 @@ class PageForm
                         ...ImageUpload::make(
                             'card_image_path',
                             'pages/card-images',
-                            'Card image (optional)',
+                            'Card image (leave empty to use Header Image)',
                             fn (ViewField $upload): ViewField => $upload
                                 ->hintIcon(
                                     Heroicon::OutlinedInformationCircle,
