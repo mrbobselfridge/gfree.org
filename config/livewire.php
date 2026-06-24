a8d5c8213edd0b44cd1f8ecab9e52b1790ed0d8f
mrbobselfridge <selfridge@gmail.com>
2026-06-23 21:40:28 -0400

    2026-06-23 21:40:26

--- config/livewire.php
+++ config/livewire.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    'temporary_file_upload' => [
+        'rules' => ['required', 'file', 'max:102400'],
+        'max_upload_time' => 10,
+    ],
+
     'payload' => [
         'max_size' => 1024 * 1024,
         'max_nesting_depth' => 30,
